e598839cd0a1c57ac89d93e8e14a537093852624
Raman.r <raman.r@inspireinfosol.com>
2026-02-05 15:22:52 +0530

    statics issue fixed

--- obj/Debug/net8.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c6f3278d81c9b80111370606b1e8e74a9291fcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17417aeb9f91e9b2b495ea1c944e92716f407cc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
